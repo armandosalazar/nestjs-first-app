d8f15a483442c3bbdeca002a0743665a247e6f1c
Armando Salazar <armandosalazar351@gmail.com>
2023-03-30 01:15:12 -0600

    add module controller and service

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.entity';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [
+    {
+      id: '1',
+      title: 'Task 1',
+      description: 'Description 1',
+      status: TaskStatus.PENDING,
+    },
+  ];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: Math.random().toString(),
+      title,
+      description,
+      status: TaskStatus.PENDING,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+}
