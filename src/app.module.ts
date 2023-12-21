d3aa442a7cbcf96ff37ebb6169ebd909cf40c562
Armando Salazar <armandosalazar351@gmail.com>
2023-12-20 22:38:38 -0600

    refactor: Remove unnecessary files and update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [TasksModule], // Permite importar otros módulos
-  controllers: [AppController], // Permite importar controladores: Se definen las rutas con las que cuenta este módulo.
-  providers: [AppService], // Permite importar servicios
+  controllers: [], // Permite importar controladores: Se definen las rutas con las que cuenta este módulo.
+  providers: [], // Permite importar servicios
 })
 export class AppModule {}
